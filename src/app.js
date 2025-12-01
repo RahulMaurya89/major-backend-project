29626c5e42b1cafc271ab8b86d7346bc677377fb
Teammaya-labs <ishank.github@gmail.com>
2025-12-01 09:19:06 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import userRouter from "./routes/user.routes.js";
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+app.use("/api/v1/users", userRouter);
 
-}))
-
-app.use(express.json(limit='16kb'));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public'));
-app.use(cookieParser());
-
-
-
-
-export default app;
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,64 +1,17 @@
-import express from "express";
-const app = express();
-// require ('dotenv').config({ptah: './.env'});
-
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';   // Your real express app
 
 dotenv.config({
-    path : './.env'
-})
-
+    path: './.env'
+});
 
 connectDB()
 .then(() => {
-    app.listen (process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running on port ${process.env.PORT || 8000}`);
     });
 })
 .catch((err) => {
-    console.log("Mongo connnection failed!!!!", err);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-import express from 'express';
-const app = express();
-
-(async () => {
-
-    try {
-
-         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,)
-         app.on("error", (error) => {
-            console.log("Error connecting to the database", error);
-         });
-
-         app.listen (process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-         });
-
-    } catch (error) {
-        console.error('Error connecting to the database', error);
-        throw error;
-    }
-
-})()
-*/
+    console.log("Mongo connection failed!!!!", err);
+});
